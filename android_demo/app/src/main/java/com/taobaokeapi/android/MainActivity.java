94abfdc0397255061e798abba2c6d69236266596
techbin <8@8l.ee>
2019-12-07 23:13:26 +0800

    v1

---
+++ android_demo/app/src/main/java/com/taobaokeapi/android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.taobaokeapi.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = findViewById(R.id.webview);
+        webview.loadUrl("http://127.0.0.1:3000");
+    }
+}
